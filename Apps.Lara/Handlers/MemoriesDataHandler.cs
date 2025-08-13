4631d5ad2f58a0987a8a0df055a26c80dab60c2e
ce-nistal <cecilia.nistal@blackbird.io>
2025-08-13 16:12:34 -0300

    Removed Beta tag

--- Apps.Lara/Handlers/MemoriesDataHandler.cs
+++ Apps.Lara/Handlers/MemoriesDataHandler.cs
@@ -4,19 +4,18 @@ using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using RestSharp;
 
-namespace Apps.Lara.Handlers
+namespace Apps.Lara.Handlers;
+
+public class MemoriesDataHandler(InvocationContext invocationContext) : Invocable(invocationContext), IAsyncDataSourceItemHandler
 {
-    public class MemoriesDataHandler(InvocationContext invocationContext) : Invocable(invocationContext), IAsyncDataSourceItemHandler
+    public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
     {
-        public async Task<IEnumerable<DataSourceItem>> GetDataAsync(DataSourceContext context, CancellationToken cancellationToken)
-        {
-            var laraClient = new LaraClient(Creds);
-            var request = new RestRequest("/memories", Method.Get);
-            var response = await laraClient.ExecuteWithErrorHandling<MemoriesResponseDto>(request);
+        var laraClient = new LaraClient(Creds);
+        var request = new RestRequest("/memories", Method.Get);
+        var response = await laraClient.ExecuteWithErrorHandling<MemoriesResponseDto>(request);
 
-            var memories = response.Content ?? new List<MemoryItem>();
+        var memories = response.Content ?? new List<MemoryItem>();
 
-            return memories.Select(m => new DataSourceItem(value: m.Id, displayName: m.Name));
-        }
+        return memories.Select(m => new DataSourceItem(value: m.Id, displayName: m.Name));
     }
 }
